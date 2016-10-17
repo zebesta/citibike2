2aa30ccb77923fef66811fdd35f783ba96150c03
zebesta <chris.sebesta@gmail.com>
2016-10-17 11:20:21 -0400

    added some basic google maps functionality, need to play around with it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { TravelOptionsComponent } from './travel-options/travel-options.component';
 import { TravelMethodComponent } from './travel-method/travel-method.component';
+import { MapComponent } from './map/map.component';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     TravelOptionsComponent,
-    TravelMethodComponent
+    TravelMethodComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCew4HQlvQXau-LP9DIIDG-mwSe2WtTBXM'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  title: string = 'My first angular2-google-maps project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
