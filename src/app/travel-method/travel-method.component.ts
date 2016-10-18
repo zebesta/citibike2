518b82cd0122fe35bd077be4b63bf60f7fb4c63d
zebesta <chris.sebesta@gmail.com>
2016-10-18 15:30:24 -0400

    added total time calculation to top of panel for transportation methods

--- src/app/travel-method/travel-method.component.ts
+++ src/app/travel-method/travel-method.component.ts
@@ -10,6 +10,8 @@ import { Travelcard } from '../travelcard';
 export class TravelMethodComponent implements OnInit {
   showMap: boolean = false;
   selectedTc: Travelcard;
+  totalTime: number =0;
+  totalTimeString: string;
 
   @Input() input: Transmethod;
 
@@ -17,6 +19,10 @@ export class TravelMethodComponent implements OnInit {
 
   ngOnInit() {
     console.log(JSON.stringify(this.input.travelCards));
+    for(let t of this.input.travelCards){
+      this.totalTime += t.time;
+    }
+    this.totalTimeString = this.formatTime(this.totalTime);
   }
   goToMap(tc: Travelcard){
     // TODO: fix this! a jank way to toggle the view and refresh the child component
@@ -25,5 +31,16 @@ export class TravelMethodComponent implements OnInit {
     // this.showMap = true;
     console.log(tc.type);
   }
+
+  formatTime(time: number){
+    var hours: number = Math.floor(time/60/60);
+    var minutes: number = Math.floor((time/60)%60);
+    var seconds: number = time%60;
+    var timeString: string  = "Total time is " + Math.floor(time/60) + " minutes and " + time%60 + " seconds";
+    if(hours){
+      timeString = "Total time is " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds";
+    }
+    return timeString;
+  };
 
 }
